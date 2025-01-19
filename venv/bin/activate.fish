14b6ff2c6ac5b18bbc984a60e0f19acf420f3e69
OEbinisa <o.ebinisa@gmail.com>
2025-01-19 14:27:58 +0100

    CI test 2

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/ebinisa/Documents/me/DevOps/VscodeDV/devops02/scrap/study02/devboot02-ab/venv"
+set -gx VIRTUAL_ENV "/Users/ebinisa/Desktop/DevOps Class/North122024/devboot02-ab/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
